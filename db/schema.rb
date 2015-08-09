9da8697f209f3ad8b748cb05e89b6bf108d2ed3d
Neil Prendergast <nprendergast215@gmail.com>
2015-08-09 15:44:37 -0400

    Finish toy app

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806024128) do
+ActiveRecord::Schema.define(version: 20150809192531) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.string   "user_id"
+    t.string   "integer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
